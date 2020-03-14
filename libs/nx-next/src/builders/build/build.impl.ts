21ad01f5b472a35ea86b16fbbe2dd0d01671b5ae
Spencer Elliott <me@elliottsj.com>
2020-03-14 15:33:09 -0400

    Implement '@yolkai/nx-next:build' builder

--- libs/nx-next/src/builders/build/build.impl.ts
+++ libs/nx-next/src/builders/build/build.impl.ts
@@ -1,29 +1,25 @@
-import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/architect';
+import {
+  BuilderContext,
+  BuilderOutput,
+  createBuilder
+} from '@angular-devkit/architect';
 import build from 'next/dist/build';
-import { PHASE_PRODUCTION_BUILD } from 'next/dist/next-server/lib/constants';
 import * as path from 'path';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { prepareConfig } from '../../utils/config';
-import { NextBuildBuilderOptions } from '../../utils/types';
+import { Observable, from } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { BuildBuilderSchema } from './schema';
 
-try {
-  require('dotenv').config();
-} catch (e) {}
-
-export default createBuilder<NextBuildBuilderOptions>(run);
-
-export function run(
-  options: NextBuildBuilderOptions,
-  context: BuilderContext,
+export function runBuilder(
+  options: BuildBuilderSchema,
+  context: BuilderContext
 ): Observable<BuilderOutput> {
   const root = path.resolve(context.workspaceRoot, options.root);
-  const config = prepareConfig(
-    context.workspaceRoot,
-    options.root,
-    options.outputPath,
-    options.fileReplacements,
-    PHASE_PRODUCTION_BUILD,
+  return from(build(root)).pipe(
+    map(() => ({ success: true })),
+    tap(() => {
+      context.logger.info('Build complete.');
+    })
   );
-  return from(build(root, config as any)).pipe(map(() => ({ success: true })));
 }
+
+export default createBuilder(runBuilder);
